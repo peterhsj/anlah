d20fcb2fa66272528c8b892ea41ae478c5dd6acd
peterhsj <peterhsj@gmail.com>
2018-08-22 03:07:30 +0800

    便利包

--- frontend/base/js/base.js
+++ frontend/base/js/base.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     //引入各元件
     $('#headerTxt').load('header.html');
+    $('#headerContent').load('header_content.html');
     $('#mainMenuTxt').load('mainMenu.html');
     $('#fatFooterTxt').load('fatFooter.html');
     $('#footerTxt').load('footer.html');
@@ -115,6 +116,118 @@ $(document).ready(function () {
             },
             "aria": {
                 "sortAscending":  ": 升冪排列",
+                "sortDescending": ": 降冪排列"
+            }
+        },
+        "pagingType": "full_numbers",
+        "ordering": false
+        // ,
+        // "searching": false
+    });
+    $('#myTable4').DataTable({
+        "language": {
+            "processing": "處理中...",
+            "loadingRecords": "載入中...",
+            "lengthMenu": "顯示 _MENU_ 筆結果",
+            "zeroRecords": "沒有符合的結果",
+            "info": "顯示第 _START_ 至 _END_ 筆結果，共 _TOTAL_ 筆",
+            "infoEmpty": "顯示第 0 至 0 筆結果，共 0 筆",
+            "infoFiltered": "(從 _MAX_ 筆結果中過濾)",
+            "infoPostFix": "",
+            "search": "搜尋:",
+            "searchPlaceholder": "請輸入關鍵字",
+            "paginate": {
+                "first": "第一頁",
+                "previous": "上一頁",
+                "next": "下一頁",
+                "last": "最後一頁"
+            },
+            "aria": {
+                "sortAscending": ": 升冪排列",
+                "sortDescending": ": 降冪排列"
+            }
+        },
+        "pagingType": "full_numbers",
+        "ordering": false
+        // ,
+        // "searching": false
+    });
+    $('#myTable5').DataTable({
+        "language": {
+            "processing": "處理中...",
+            "loadingRecords": "載入中...",
+            "lengthMenu": "顯示 _MENU_ 筆結果",
+            "zeroRecords": "沒有符合的結果",
+            "info": "顯示第 _START_ 至 _END_ 筆結果，共 _TOTAL_ 筆",
+            "infoEmpty": "顯示第 0 至 0 筆結果，共 0 筆",
+            "infoFiltered": "(從 _MAX_ 筆結果中過濾)",
+            "infoPostFix": "",
+            "search": "搜尋:",
+            "searchPlaceholder": "請輸入關鍵字",
+            "paginate": {
+                "first": "第一頁",
+                "previous": "上一頁",
+                "next": "下一頁",
+                "last": "最後一頁"
+            },
+            "aria": {
+                "sortAscending": ": 升冪排列",
+                "sortDescending": ": 降冪排列"
+            }
+        },
+        "pagingType": "full_numbers",
+        "ordering": false
+        // ,
+        // "searching": false
+    });
+    $('#myTable6').DataTable({
+        "language": {
+            "processing": "處理中...",
+            "loadingRecords": "載入中...",
+            "lengthMenu": "顯示 _MENU_ 筆結果",
+            "zeroRecords": "沒有符合的結果",
+            "info": "顯示第 _START_ 至 _END_ 筆結果，共 _TOTAL_ 筆",
+            "infoEmpty": "顯示第 0 至 0 筆結果，共 0 筆",
+            "infoFiltered": "(從 _MAX_ 筆結果中過濾)",
+            "infoPostFix": "",
+            "search": "搜尋:",
+            "searchPlaceholder": "請輸入關鍵字",
+            "paginate": {
+                "first": "第一頁",
+                "previous": "上一頁",
+                "next": "下一頁",
+                "last": "最後一頁"
+            },
+            "aria": {
+                "sortAscending": ": 升冪排列",
+                "sortDescending": ": 降冪排列"
+            }
+        },
+        "pagingType": "full_numbers",
+        "ordering": false
+        // ,
+        // "searching": false
+    });
+    $('#myTable7').DataTable({
+        "language": {
+            "processing": "處理中...",
+            "loadingRecords": "載入中...",
+            "lengthMenu": "顯示 _MENU_ 筆結果",
+            "zeroRecords": "沒有符合的結果",
+            "info": "顯示第 _START_ 至 _END_ 筆結果，共 _TOTAL_ 筆",
+            "infoEmpty": "顯示第 0 至 0 筆結果，共 0 筆",
+            "infoFiltered": "(從 _MAX_ 筆結果中過濾)",
+            "infoPostFix": "",
+            "search": "搜尋:",
+            "searchPlaceholder": "請輸入關鍵字",
+            "paginate": {
+                "first": "第一頁",
+                "previous": "上一頁",
+                "next": "下一頁",
+                "last": "最後一頁"
+            },
+            "aria": {
+                "sortAscending": ": 升冪排列",
                 "sortDescending": ": 降冪排列"
             }
         },
